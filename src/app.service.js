42b685b076f354038b34f0ebf2f9f3aaa1eae6e7
Giuseppe <giuse.scara@gmail.com>
2019-12-23 15:19:47 +0100

    Apply MCV pattern to clean code and test

---
+++ src/app.service.js
@@ -0,0 +1,30 @@
+function RockPaperScissorsService() {
+  const beatsStrategy = {
+    1: [2],
+    2: [3],
+    3: [1]
+  };
+
+  const checkGameWinner = function(strategy, choise1, choise2) {
+    return strategy[choise1].includes(choise2) ? choise1 : choise2;
+  };
+
+  const generateComputerChoise = function(gameElementsToCheck) {
+    var computerChoise = 0;
+    const randomChoise = Math.random();
+    for (const index in gameElementsToCheck) {
+      if (randomChoise < gameElementsToCheck[index].generationRange) {
+        computerChoise = gameElementsToCheck[index].id;
+      }
+    }
+    return computerChoise;
+  };
+
+  return {
+    beatsStrategy: beatsStrategy,
+    generateComputerChoise: generateComputerChoise,
+    checkGameWinner: checkGameWinner
+  };
+};
+
+module.exports = RockPaperScissorsService;
